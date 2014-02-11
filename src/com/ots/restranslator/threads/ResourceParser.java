929c79621984dfe1cc7d30402c5fcc8b2ea3e844
Jafar <jqaddoumi@otsdc.com>
2014-02-11 15:11:11 +0200

    bla

---
+++ src/com/ots/restranslator/threads/ResourceParser.java
@@ -0,0 +1,16 @@
+package com.ots.restranslator.threads;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+/**
+ * Created by jafar on 2/11/14.
+ */
+public class ResourceParser extends Thread {
+
+    public ResourceParser(){
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        //factory.
+       // DocumentBuilder builder = factory.newDocumentBuilder();
+    }
+}
