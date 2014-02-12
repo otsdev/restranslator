3269487629ab5f34dc7b8e6b93a523ff78f24fd6
Jafar <jqaddoumi@otsdc.com>
2014-02-12 16:10:50 +0200

    xml parser

--- src/com/ots/restranslator/xml/XmlResourceParser.java
+++ src/com/ots/restranslator/xml/XmlResourceParser.java
@@ -1,10 +1,73 @@
 package com.ots.restranslator.xml;
 
+import com.ots.restranslator.models.StringResources;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
+
 /**
  * Created by jafar on 2/11/14.
  */
 public class XmlResourceParser {
 
+    private StringResources mStringResources;
+
     public XmlResourceParser() {
+        mStringResources = new StringResources();
+    }
+
+    public void parse(String path) {
+        parse(new File(path));
+    }
+
+    public void parse(File file) {
+        try {
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+            ResourceHandler handler = new ResourceHandler();
+            InputStream is = new FileInputStream(file);
+            parser.parse(is, handler);
+        } catch (ParserConfigurationException e) {
+
+        } catch (SAXException e) {
+
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+
+    public class ResourceHandler extends DefaultHandler {
+
+        @Override
+        public void startDocument() throws SAXException {
+            super.startDocument();
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            super.startElement(uri, localName, qName, attributes);
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            super.characters(ch, start, length);
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            super.endElement(uri, localName, qName);
+        }
+
+        @Override
+        public void endDocument() throws SAXException {
+            super.endDocument();
+        }
     }
 }
