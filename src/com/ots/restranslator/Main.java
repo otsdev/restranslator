833668c3ddb213c9ad3865945a260813d91eabb2
Mahdi Hijazi <mhijazi@otsdc.com>
2014-02-14 15:52:52 +0300

    load model data inside table view

--- src/com/ots/restranslator/Main.java
+++ src/com/ots/restranslator/Main.java
@@ -1,18 +1,25 @@
 package com.ots.restranslator;
 
 import com.ots.restranslator.frames.MainFrame;
+import javax.swing.UIManager;
 
 /**
  * Created by jafar on 2/5/14.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
                 MainFrame mainFrame = new MainFrame();
                 mainFrame.setSize(800, 600);
                 mainFrame.setLocationRelativeTo(null);
--- src/com/ots/restranslator/frames/MainFrame.java
+++ src/com/ots/restranslator/frames/MainFrame.java
@@ -1,23 +1,42 @@
 package com.ots.restranslator.frames;
 
+import com.ots.restranslator.models.StringResourceItem;
 import com.ots.restranslator.models.StringResources;
 import com.ots.restranslator.xml.XmlResourceParser;
 import com.ots.translator.dialogs.OpenFileDialog;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
 
 /**
  * Created by jafar on 2/6/14.
  */
 public class MainFrame extends JFrame {
 
+    JTable table;
+    public static final String[] COLUMNS_NAMES = {"Key",
+        "default",
+        "translation"};
+
     public MainFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         setJMenuBar(createMenuBar());
+        initComponents();
+    }
+
+    private void initComponents() {
+        table = new JTable();
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+
+        setLayout(new BorderLayout());
+        add(scrollPane, BorderLayout.CENTER);
 
     }
 
@@ -52,6 +71,22 @@ public class MainFrame extends JFrame {
                 KeyEvent.VK_O, ActionEvent.ALT_MASK));
         menu.add(menuItem);
 
+        menuItem = new JMenuItem("Save",
+                KeyEvent.VK_S);
+
+        menuItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StringResources stringResources = ((StringResourcesModel)table.getModel()).getStringResources();
+                System.err.println(stringResources.flatten());
+                
+            }
+        });
+
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_S, ActionEvent.ALT_MASK));
+        menu.add(menuItem);
         return menuBar;
     }
 
@@ -64,7 +99,12 @@ public class MainFrame extends JFrame {
             parser.parse(translateFile, false);
 
             StringResources resources = parser.getmStringResources();
-            System.out.print(resources.flatten());
+//            System.out.print(resources.flatten());
+
+            final StringResourcesModel model = new StringResourcesModel(resources);
+            table.setModel(model);
+            pack();
+
         }
 
         @Override
@@ -72,4 +112,106 @@ public class MainFrame extends JFrame {
             System.err.println("File selection canceled");
         }
     };
+
+    class StringResourcesModel extends AbstractTableModel {
+
+        private StringResources mStringResources;
+        private static final int COLUMN_KEY = 0;
+        private static final int COLUMN_ORIGINAL = 1;
+        private static final int COLUMN_TRANSLATED = 2;
+
+        public StringResourcesModel(StringResources stringResources) {
+            this.mStringResources = stringResources;
+            System.err.println("count: " + getRowCount());
+
+            System.err.println("item at 0, 0: " + getValueAt(0, 0));
+
+        }
+
+        public int getColumnCount() {
+            return COLUMNS_NAMES.length;
+
+        }
+
+        public int getRowCount() {
+            return mStringResources.getItemsCount();
+
+        }
+
+        public String getColumnName(int col) {
+            return COLUMNS_NAMES[col];
+        }
+
+        public Object getValueAt(int row, int col) {
+            System.err.println("data at: " + col + ", " + row);
+
+            switch (col) {
+                case COLUMN_KEY:
+                    return mStringResources.getItemKeyAt(row);
+                case COLUMN_ORIGINAL:
+                    return mStringResources.getItemOriginalTextAt(row);
+
+                case COLUMN_TRANSLATED:
+                    return mStringResources.getItemTranslatedTextAt(row);
+
+                default:
+                    throw new IllegalStateException("invalid column index");
+            }
+
+        }
+
+        /*
+         * JTable uses this method to determine the default renderer/
+         * editor for each cell.  If we didn't implement this method,
+         * then the last column would contain text ("true"/"false"),
+         * rather than a check box.
+         */
+        public Class getColumnClass(int c) {
+            return String.class;
+        }
+
+        /*
+         * Don't need to implement this method unless your table's
+         * editable.
+         */
+        public boolean isCellEditable(int row, int col) {
+            //Note that the data/cell address is constant,
+            //no matter where the cell appears onscreen.
+//            if (col < 2) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+
+            return true;
+        }
+
+        /*
+         * Don't need to implement this method unless your table's
+         * data can change.
+         */
+        public void setValueAt(Object value, int row, int col) {
+            switch (col) {
+                case COLUMN_ORIGINAL:
+                    mStringResources.setOriginalText(mStringResources.getItemKeyAt(row), (String) value);
+
+                    break;
+                case COLUMN_TRANSLATED:
+                    mStringResources.setTranslatedText(mStringResources.getItemKeyAt(row), (String) value);
+
+                    break;
+                default:
+                    throw new IllegalStateException("invalid column index");
+            }
+
+            fireTableCellUpdated(row, col);
+
+        }
+
+        public StringResources getStringResources() {
+            return this.mStringResources;
+
+        }
+
+    }
 }
